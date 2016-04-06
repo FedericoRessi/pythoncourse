a7e98f331297bb63221e7fef3dde018561df9288
Federico Ressi <federico.ressi@intel.com>
2016-04-06 15:12:50 +0200

    python executable version.

---
+++ python_class_2016_01_01/module_01_hello_world.py
@@ -0,0 +1,7 @@
+'''
+Created on 6 Apr 2016
+
+@author: Federico Ressi <federico.ressi@intel.com>
+'''
+
+# Write below a line that prints 'Hello world!'
---
+++ python_class_2016_01_01/tests/test_00_setup.py
@@ -0,0 +1,20 @@
+'''
+Created on 6 Apr 2016
+
+@author: Federico Ressi <federico.ressi@intel.com>
+'''
+
+import sys
+import unittest
+
+
+class TestSetup(unittest.TestCase):
+
+    def test_python_version(self):
+        version = sys.version.split()[0].rsplit('.', 1)[0]
+        assert version in ['2.7', '3.4']
+
+    def test_default_python_interpreter_version(self):
+        from sh import bash
+        version = str(bash('-c', 'python -c "import sys; print(sys.version)"'))
+        assert sys.version == version.rsplit('\n', 1)[0]

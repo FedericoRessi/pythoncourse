fae324501f8cb95d70bf63c72298bfcd921f27ba
Federico Ressi <federico.ressi@intel.com>
2016-04-25 16:36:34 +0100

    Exercise for while loop.

---
+++ python_course_1604/class_02/exercize_01_while_loop.py
@@ -0,0 +1,29 @@
+'''
+Created on 6 Apr 2016
+
+@author: Federico Ressi <federico.ressi@intel.com>
+'''
+
+
+def is_prime(number):
+    """ returns True if given number is prime
+    """
+    i = 2
+    while i < number:
+        if number % i == 0:
+            return False
+        i += 1
+
+    return True
+
+
+def print_primes(max_number):
+    """ Prints al prime numbers that are smaller than max_number
+
+    To look if a number is prime it calls is_prime function.
+    """
+    i = 1
+    while i < max_number:
+        if is_prime(i):
+            print(i)
+        i += 1

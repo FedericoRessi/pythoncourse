9e5abd0e88b4502a62b1f6e0e496d0d9c96ce639
Barry Duggan <bduggan@omalleypark-1.ir.intel.com>
2016-04-25 17:22:44 +0100

    Completed "if" statement exercise

--- python_course_1604/class_01/exercize_02_if_statement.py
+++ python_course_1604/class_01/exercize_02_if_statement.py
@@ -8,11 +8,10 @@ Created on 6 Apr 2016
 
 
 def clamp(value, min_value, max_value):
-    """Function that:
 
-    - Returns min_value when value is smaller than min_value
-    - Returns max_value when value is smaller than max_value
-    - Returns value in all other cases
-
-    Please use if, elif and else keywords
-    """
+    if value < min_value:
+        return min_value
+    elif value > max_value:
+        return max_value
+    else:
+        return value

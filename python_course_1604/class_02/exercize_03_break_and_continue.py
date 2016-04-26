94b474dbd13b80969f6408e53d02451c0cf5dbe0
Aljeddani <aljeddani99@gmail.com>
2016-04-27 00:47:55 +0100

    "Understanding the question is half the answer"

--- python_course_1604/class_02/exercize_03_break_and_continue.py
+++ python_course_1604/class_02/exercize_03_break_and_continue.py
@@ -19,16 +19,18 @@ def execute_commands(commands):
 
     Please use both continue and break to complete this exercize
     """
-
+    skip_flag = False
+    
     for command in commands:
         # HINT: implement skip and quit commands here before other commands
-        if command == 'skip':
+        if skip_flag:
+            skip_flag = False
             continue
-        
-        if command == 'quit':
+        elif command == 'skip':
+            skip_flag = True
+        elif command == 'quit':
             break
-        
-        if command == 'ping':
+        elif command == 'ping':
             print('pong')
         else:
             print('error')

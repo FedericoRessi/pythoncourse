8ebb8f1d38bbd8c1b854003a3a1819b8074375d9
Federico Ressi <federico.ressi@intel.com>
2016-04-26 19:38:12 +0100

    Revert "Gstedje"

--- python_course_1604/class_01/exercize_04_stack_limit.py
+++ python_course_1604/class_01/exercize_04_stack_limit.py
@@ -2,33 +2,29 @@
 Created on 6 Apr 2016
 
 @author: Federico Ressi <federico.ressi@intel.com>
-@modified by: Gustav Stedje <gustav.stedje@intel.com>
 '''
 
-# Complete recursive_func function to satisfy documented behaviour
+# Complete recursive_func function to sarisfy documented behaviour
 
 
 def measure_stack_limit():
     """Function that measures actual stack limit.
 
-    This function calls a new recursive function to reach stack limit and
+    This function calls a new recoursive function to reach stack limit and
     counts the number of calls.
 
-    It returns the number of recursive functions calls before stack limit is
+    It returns the number of recoursive functions calls before stack limit is
     reached.
     """
+
     return recursive_function(0)
 
 
 def recursive_function(level):
-    """Function that recursively calls himself with level + 1 as parameter
+    """Function that recursively calls himseld with level + 1 as parameter
 
     It returns level when stack limit is reached.
     It returns the result of recursive call if limit is not reached.
 
     Hints: when stack limit is reached RuntimeError is raised.
     """
-    try:
-        return recursive_function(level+1)
-    except RuntimeError:
-        return level

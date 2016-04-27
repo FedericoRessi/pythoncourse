d89ada5b80ea0b66b8ab4878a00332f7da70edd7
John Flaherty <john.flaherty@intel.com>
2016-04-27 14:09:55 +0100

    Exercise 4 from Class 1 - Stack limit

--- python_course_1604/class_01/exercize_04_stack_limit.py
+++ python_course_1604/class_01/exercize_04_stack_limit.py
@@ -28,3 +28,7 @@ def recursive_function(level):
 
     Hints: when stack limit is reached RuntimeError is raised.
     """
+    try:
+        return recursive_function(level+1)
+    except RuntimeError:
+        return level

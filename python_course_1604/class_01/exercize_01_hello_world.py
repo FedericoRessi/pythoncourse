92cf3c45b4df1c74d7f7e955f94ad14528cbac6c
root <root@silvesf2232b101.ir.intel.com>
2016-04-29 14:42:51 +0100

    Completed exercise 01, Class 01

--- python_course_1604/class_01/exercize_01_hello_world.py
+++ python_course_1604/class_01/exercize_01_hello_world.py
@@ -2,6 +2,9 @@
 Created on 6 Apr 2016
 
 @author: Federico Ressi <federico.ressi@intel.com>
+
+Task: Write a line that prints Hello world!
+ YOU MUST BE SPECIFIC TO PASS THE TESTS!
 '''
 
-# Write a line that prints 'Hello world!'
+print ('Hello world!')
